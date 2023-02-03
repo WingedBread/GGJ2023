efe3e4e1d445ec49d1d5e59c90f540c6ac7fc1ef
Carlos Orellana <carlos.orellana.sge@mango.com>
2023-02-03 17:42:53 +0100

    card hoe done

--- Assets/Scripts/Cards/Card Types/CardHoe.cs
+++ Assets/Scripts/Cards/Card Types/CardHoe.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class CardHoe : Card
 {
+    public Tile farmeableTile;
+
     public override bool play(Tile clickedTile){
         Debug.Log("HOE USED");
+        GridManager.Instance.SetTile(clickedTile.transform.position, farmeableTile);
         return true;
     }
 }
