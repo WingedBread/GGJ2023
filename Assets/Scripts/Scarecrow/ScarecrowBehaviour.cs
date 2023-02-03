4aa909862e8954651e7d091bb6a78b84620508e9
Jesús Martínez <jmh952010@gmail.com>
2023-02-04 00:24:45 +0100

    scarecrow wip

---
+++ Assets/Scripts/Scarecrow/ScarecrowBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScarecrowBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject area;
+
+    private List<Vector2> areaLocations = new List<Vector2>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        //areaCollider = area.GetComponent<BoxCollider2D>();
+        FillScarecrowAreaLocation();
+    }
+
+    void FillScarecrowAreaLocation()
+    {
+        areaLocations.Add(new Vector2(transform.position.x + 1, transform.position.y));
+        areaLocations.Add(new Vector2(transform.position.x - 1, transform.position.y));
+        areaLocations.Add(new Vector2(transform.position.x, transform.position.y+1));
+        areaLocations.Add(new Vector2(transform.position.x, transform.position.y - 1));
+        areaLocations.Add(new Vector2(transform.position.x + 1, transform.position.y + 1));
+        areaLocations.Add(new Vector2(transform.position.x - 1, transform.position.y - 1));
+        areaLocations.Add(new Vector2(transform.position.x + 1, transform.position.y - 1));
+        areaLocations.Add(new Vector2(transform.position.x - 1, transform.position.y + 1));
+        areaLocations.Add(new Vector2(transform.position.x, transform.position.y));
+    }
+
+    public List<Vector2> GetAreaLocations()
+    {
+        return areaLocations;
+    }
+
+    public void DestroyScarecrow()
+    {
+        Destroy(this);
+    }
+}
